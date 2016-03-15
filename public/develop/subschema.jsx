3ab3692b8bafd06d99e39521b2c3763ac89b9c75
juspears <juspears@paypal.com>
2016-03-15 14:31:07 -0700

    missing fbjs

---
+++ public/develop/subschema.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function SubschemaDevelop(props) {
+
+    return (<div>
+        <h3>Subschema Development</h3>
+        <fieldset>
+            <p className="lead">
+                see <a href="https://github.com/subschema/subschema-devel">github</a> for a project to help contribute
+                to subschema.
+            </p>
+        </fieldset>
+    </div>);
+
+}
