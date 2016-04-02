548370ce4ea7209f2ea313ee4e23e883715d8445
juspears <juspears@paypal.com>
2016-04-01 17:29:25 -0700

    Added more info to schema

--- public/develop/schema.jsx
+++ public/develop/schema.jsx
@@ -1,10 +1,32 @@
 var React = require('react');
 var example = JSON.stringify({
         schema: {
-            'field': {}
+            'field': {
+                type: "Text",
+                title: "The Label (false for no label)",
+                validators: [{
+                    type: "required",
+                    message: "The message is optional, type isn't, no validators no validation"
+                }],
+                template: {
+                    "template": "EditorTemplate (is the default)",
+                    "className": "Any other property described in the templates PropTypes"
+                },
+                conditional: {
+                    listen: "path.to.value",
+                    operator: '==',
+                    value: true
+                }
+            }
         },
         fieldsets: [{
-            fields: []
+            legend: 'The legend for fieldset (optional)',
+            content: "A content object that will display content",
+            template: "The template to use (By Default uses FieldSetTemplate)",
+            fields: "field a comma delimited string or an array of strings",
+            fieldsets: [{
+                legend: "Recursively define a fieldset using the schema.  Short circuits on fieldsets with fields."
+            }]
         }]
     },
     null, '\t');
